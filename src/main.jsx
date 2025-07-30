01096fb18070c63562669b6bb8f7cab1ed1d7f62
Abdelrahmanjr246 <am443814@gmail.com>
2025-07-30 14:19:51 +0200

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/outfit';
+import { BrowserRouter } from 'react-router-dom'
+import '@fontsource-variable/outfit'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/E-commerce">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
