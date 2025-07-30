0d8a3a24d83ea791029a43194840987d142b8cf6
Abdelrahmanjr246 <am443814@gmail.com>
2025-07-30 14:05:29 +0200

    add 404 page

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
   base: mode === 'production' ? '/E-commerce/' : '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  }
 }))
